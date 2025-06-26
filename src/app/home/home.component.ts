3684bc47d78de96cc54b7f3e6d459c5b4cd15682
Ivan Rodrigues <ivanho92@hotmail.be>
2025-06-26 18:42:05 +0200

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: ` <p>HomeComponent</p> `,
+  styles: ``,
+})
+export default class HomeComponent {}
