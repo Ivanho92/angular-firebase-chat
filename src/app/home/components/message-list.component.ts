4b4a9b122fd689437e002039b8300138d818bbfa
Ivan Rodrigues <ivanho92@hotmail.be>
2025-07-14 20:14:51 +0200

    feat: styling

--- src/app/home/components/message-list.component.ts
+++ src/app/home/components/message-list.component.ts
@@ -1,12 +1,19 @@
-import { Component, input } from '@angular/core';
+import { CdkScrollable } from '@angular/cdk/overlay';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { Component, effect, input, viewChild } from '@angular/core';
 import { Message } from '@app/core/models/message.model';
+import { AuthUser } from '@app/core/services/auth.service';
 
 @Component({
   selector: 'app-message-list',
   template: `
-    <ul class="gradient-bg">
+    <ul class="gradient-bg" cdkScrollable>
       @for (message of messages(); track message.created) {
-        <li>
+        <li
+          [style.flex-direction]="
+            message.author === activeUser()?.email ? 'row-reverse' : 'row'
+          "
+        >
           <div class="avatar animate-in-primary">
             <img
               src="https://api.dicebear.com/7.x/thumbs/svg?seed={{
@@ -24,39 +31,52 @@ import { Message } from '@app/core/models/message.model';
       }
     </ul>
   `,
-  styles: [
-    `
-      ul {
-        height: 100%;
-        overflow: auto;
-        list-style-type: none;
-        padding: 1rem;
-        padding-bottom: 5rem;
-        margin: 0;
-      }
+  styles: `
+    ul {
+      height: 100%;
+      overflow: auto;
+      list-style-type: none;
+      padding: 1rem;
+      padding-bottom: 5rem;
+      margin: 0;
+    }
 
-      li {
-        display: flex;
-        margin-bottom: 2rem;
-      }
+    li {
+      display: flex;
+      margin-bottom: 2rem;
+    }
 
-      .avatar {
-        width: 75px;
-        margin: 0 1rem;
-        height: auto;
-        filter: drop-shadow(2px 3px 5px var(--accent-darker-color));
-      }
+    .avatar {
+      width: 75px;
+      margin: 0 1rem;
+      height: auto;
+      filter: drop-shadow(2px 3px 5px var(--accent-darker-color));
+    }
 
-      .message {
-        width: 100%;
-        background: var(--white);
-        padding: 2rem;
-        border-radius: 5px;
-        filter: drop-shadow(2px 4px 3px var(--primary-darker-color));
-      }
-    `,
-  ],
+    .message {
+      width: 100%;
+      background: var(--white);
+      padding: 2rem;
+      border-radius: 5px;
+      filter: drop-shadow(2px 4px 3px var(--primary-darker-color));
+    }
+  `,
+  imports: [ScrollingModule],
 })
 export class MessageListComponent {
   messages = input.required<Message[]>();
+  activeUser = input.required<AuthUser>();
+
+  scrollContainer = viewChild.required(CdkScrollable);
+
+  constructor() {
+    effect(() => {
+      if (this.messages().length && this.scrollContainer()) {
+        this.scrollContainer().scrollTo({
+          bottom: 0,
+          behavior: 'smooth',
+        });
+      }
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,11 +26,35 @@ import { MessageListComponent } from '@app/home/components/message-list.componen
         </button>
       </mat-toolbar>
 
-      <app-message-list [messages]="messageService.messages()" />
+      <app-message-list
+        [messages]="messageService.messages()"
+        [activeUser]="authService.user()"
+      />
       <app-message-input (onNewMessage)="messageService.add$.next($event)" />
     </div>
   `,
-  styles: ``,
+  styles: `
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+    }
+
+    mat-toolbar {
+      box-shadow: 0px -7px 11px 0px var(--accent-color);
+    }
+
+    app-message-list {
+      height: 100%;
+      width: 100%;
+    }
+
+    app-message-input {
+      position: fixed;
+      bottom: 0;
+    }
+  `,
 })
 export default class HomeComponent {
   protected readonly messageService = inject(MessageService);
